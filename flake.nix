125460bef9924d585eed89b8c20f1e266795373f
koyuch <michal@koyuch.dev>
2025-03-04 12:05:19 +0100

    added home + plasma -managers to flake.nix

--- flake.nix
+++ flake.nix
@@ -5,9 +5,18 @@
     # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     impermanence.url = "github:nix-community/impermanence";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = { self, nixpkgs, impermanence, ... }@inputs: {
+  outputs = { self, nixpkgs, impermanence, home-manager, plasma-manager, ... }@inputs: {
     # hostname
     nixosConfigurations.think-nix = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -16,6 +25,15 @@
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
+
+        # Add home-manager as a NixOS module
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+            home-manager.users.koyuch = import ./home.nix;
+          }
       ];
     };
   };
