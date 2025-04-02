1c04b89ef4408aef05b490ea9373ddc83456176b
koyuch <michal@koyuch.dev>
2025-04-02 10:44:58 +0200

    Enable US and Slovak keyboard layouts with Alt+Shift switching

--- configuration.nix
+++ configuration.nix
@@ -117,10 +117,12 @@
 
   # qt.style = "breeze";
 
-
   # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  services.xserver.xkb = {
+    layout = "us,sk";
+    variant = ",qwerty";
+    options = "grp:alt_shift_toggle";  # Use Alt+Shift to switch between layouts
+  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
