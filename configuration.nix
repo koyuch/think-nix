cb4f5c9a45e2a41bf9571c353001d51ef4577a1f
koyuch <michal@koyuch.dev>
2026-02-08 13:38:23 +0100

    feat: Enable OpenSSH server and allow port 22

--- configuration.nix
+++ configuration.nix
@@ -320,10 +320,10 @@
   };
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 22 ]; # SSH
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
